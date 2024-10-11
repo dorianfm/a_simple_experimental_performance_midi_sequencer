c925eef681377a173263a50ce48b3740d730a1a7
Dorian Fraser-Moore <dorian+code@theusefularts.org>
2024-10-11 17:48:52 +0100

    simplify interface, add basic drag and drop functiions

--- interface.js
+++ interface.js
@@ -4,15 +4,31 @@ let controls = document.getElementById('controls');
 let defaultTrack = document.querySelector('.track').cloneNode(true);
 let defaultPattern = document.querySelector('.pattern').cloneNode(true);
 let defaultStep = document.querySelector('.step').cloneNode(true);
+let dragSource, dragElement, dragModifierKey;
 
+if (navigator.appVersion.indexOf('Mac') != -1) {
+    dragModifierKey = 'Alt';
+} else {
+    dragModifierKey = 'Control';
+}
+tracks.addEventListener('mousedown', trackMouseDown);
+tracks.addEventListener('mouseup', trackMouseUp);
 tracks.addEventListener('click', trackClick);
+tracks.addEventListener('dragstart', trackDragStart);
+tracks.addEventListener('drag', trackDrag);
+tracks.addEventListener('dragend', trackDragEnd);
+tracks.addEventListener('dragenter', trackDragEnter);
+tracks.addEventListener('dragover', trackDragOver);
+tracks.addEventListener('dragleave', trackDragLeave);
+tracks.addEventListener('drop', trackDrop);
+
 controls.addEventListener('click', controlClick);
 
 function controlClick(evt)
 {
-    if (evt.srcElement.nodeName == 'BUTTON') {
-        controlButtonClick(evt.srcElement.value);
-    }
+    if (evt.target.nodeName == 'BUTTON') {
+        controlButtonClick(evt.target.value);
+    }  
 }
 
 function controlButtonClick(action) 
@@ -30,25 +46,98 @@ function controlTogglePlay() {
 
 function trackClick(evt)
 {
-    if (evt.srcElement.nodeName == 'BUTTON') {
-        buttonClick(evt.srcElement.value, evt.srcElement, evt);
+    if (evt.target.nodeName == 'BUTTON') {
+        buttonClick(evt.target.value, evt.target, evt);
     }
+}
+
+function trackMouseDown(evt)
+{
+    if (evt.target.matches('.handle')) {
+        evt.target.parentNode.setAttribute("draggable", "true");
+    }
+}
+
+function trackMouseUp(evt)
+{
+    clearDraggable();
+}
+
+function trackDragStart(evt)
+{
+    dragElement = evt.target;
+    evt.target.classList.add('dragging');
+}
+
+function trackDrag(evt)
+{
+    // placeholder
+}
+
+
+function trackDragEnd(evt)
+{
+    evt.target.classList.remove('dragging');
+    dragElement = false;
+    clearDraggable();
+}
+
+function trackDragEnter(evt)
+{
+    evt.preventDefault();
+    let target = evt.target.closest(elementClass(dragElement));
+    if (target) target.classList.add('dragTarget');
+}
+
+function trackDragOver(evt)
+{
+    evt.preventDefault();
+    let target = evt.target.closest(elementClass(dragElement));
+    if (target) target.classList.add('dragTarget');
+}
+
+function trackDragLeave(evt)
+{
+    let target = evt.target.closest(elementClass(dragElement));
+    if (target) target.classList.remove('dragTarget');
+}
+
+function trackDrop(evt)
+{
+    let target = evt.target.closest(elementClass(dragElement));
+    if (target) {
+        dragElement.classList.remove('dragging');
+        if (evt.getModifierState(dragModifierKey) === true) {
+            target.after(dragElement.cloneNode(true));
+        } else {
+            target.after(dragElement);
+        }
+    }
+    tracks.querySelectorAll('.dragTarget').forEach((element) => element.classList.remove('dragTarget'));
+}
+
+function clearDraggable()
+{
+    tracks.querySelectorAll("[draggable]").forEach((element) => {
+        element.setAttribute("draggable",'');
+    });
 }
 
 function buttonClick(action, button, evt)
 {
     let target = button.closest('.step, .pattern, .track');
     let targetClass = elementClass(target);
-    let newElement = evt.getModifierState('Alt') == true ? target : defaultFor(target); 
+    let newElement = evt.getModifierState(dragModifierKey) == true ? target : defaultFor(target); 
     switch(action) {
-        case 'insert before':
-            insertBefore(target, newElement);
-            break;
         case 'remove':
             remove(target);
             break;
-        case 'insert after':
-            insertAfter(target, newElement);
+        case 'insert':
+            if (evt.getModifierState('Shift')) {
+                insertBefore(target, newElement);
+            } else {
+                insertAfter(target, newElement);
+            }
             break;
     }
 }
@@ -68,7 +157,6 @@ function elementClass(target) {
 function remove(target)
 {
     let targetClass = elementClass(target);
-    console.log(target, targetClass);
     let containerClass = targetClass+'s';
     let container = target.closest(containerClass);
     let elements = container.querySelectorAll(targetClass);
